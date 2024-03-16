192fe4fee713425ee105ac1426ddecfd03120dae
Maxim Boykov <maeboykov@rasu.ru>
2024-03-16 15:15:29 +0300

    Update

--- bme_config_linux.sh
+++ bme_config_linux.sh
@@ -4,22 +4,23 @@ NVIM_VERSION=${NVIM_VERSION:-v0.9.5}
 
 
 # alias
-
 alias ff="vim +\$(rg -n . | fzf | awk -F: '{print '+'\$2,\$1}')"
 
+[ ! -d ~/.bme_tools/bin ] && mkdir -p ~/.bme_tools/bin
 
+grep -q "bme_tools/bin" ~/.bashrc || echo -e "\nexport PATH=~/.bme_tools/bin:\$PATH" >> ~/.bashrc
+ 
 # fzf
-
 [ ! -d ~/.fzf ] && (git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install)
 
-
-exit 0
-
-mkdir -p ~/.bme_tools/bin || true
-
+# nvim
+[ ! -e ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ] && \
 wget -O ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim.appimage \
+  && chmod +x ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage \
   && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/nvim \
   && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/v \
   && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/vi \
   && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/vim \
-  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/neovim \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/neovim
+
+
