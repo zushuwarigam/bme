df74625505e79fa4080564f7bd2a1a7041753448
Maxim Boykov <maeboykov@rasu.ru>
2024-03-16 14:36:00 +0300

    Add fzf

---
+++ README.md
@@ -0,0 +1,8 @@
+# Install
+```
+bash <(curl -s https://raw.githubusercontent.com/zushuwarigam/bme/main/bme_config_linux.sh)
+```
+
+# NvChad
+https://nvchad.com/docs/quickstart/install
+https://github.com/NvChad/NvChad
--- bme_config_linux.sh
+++ bme_config_linux.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-echo "BME: do my config"
+NVIM_VERSION=${NVIM_VERSION:-v0.9.5}
+
+# fzf
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 exit 0
-
-VERSION=${VERSION:-v0.9.5}
-
-apt update && apt install git wget -y
 
 mkdir -p ~/.bme_tools/bin || true
 
-wget -O ~/.bme_tools/bin/nvim_$VERSION.appimage https://github.com/neovim/neovim/releases/download/$VERSION/nvim.appimage \
-  && ln -sf ~/.bme_tools/bin/nvim_$VERSION.appimage ~/.bme_tools/bin/nvim \
-  && ln -sf ~/.bme_tools/bin/nvim_$VERSION.appimage ~/.bme_tools/bin/v \
-  && ln -sf ~/.bme_tools/bin/nvim_$VERSION.appimage ~/.bme_tools/bin/vi \
-  && ln -sf ~/.bme_tools/bin/nvim_$VERSION.appimage ~/.bme_tools/bin/vim \
-  && ln -sf ~/.bme_tools/bin/nvim_$VERSION.appimage ~/.bme_tools/bin/neovim \
+wget -O ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim.appimage \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/nvim \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/v \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/vi \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/vim \
+  && ln -sf ~/.bme_tools/bin/nvim_$NVIM_VERSION.appimage ~/.bme_tools/bin/neovim \
